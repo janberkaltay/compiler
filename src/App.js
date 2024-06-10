96800bbf9998415f5c244f7b075907891e6837a6
Janberk <jnmrtaltay38@gmail.com>
2024-06-10 10:48:54 +0300

    css bugs fixed for mobile

--- src/App.js
+++ src/App.js
@@ -148,7 +148,6 @@ function App() {
           </div>
         )}
         <div className='dev'>
-      &copy; 2024 Powered by 
                     <a href="http://www.janberkaltay.dev" target="_blank" rel="noopener noreferrer"> Janberk Altay</a>  
                 </div>
       </header>
